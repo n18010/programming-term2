6474eee6c485dbe577044079a0f2a57a9a8eb034
n18010 <n18010@std.it-college.ac.jp>
2018-07-07 22:15:26 +0900

    スペルミス修正

--- src/task20180705.py
+++ src/task20180705.py
@@ -1,11 +1,11 @@
 # 支払い金額を求める
 # 価格
-beal_v = 200
+beer_v = 200
 otumami_v = 100
 yakitori_v = 100
 
 # 個数
-beal_c = 2
+beer_c = 2
 otumami_c = 1
 yakitori_c = 2
 
@@ -16,10 +16,10 @@ yakitori_rate = 0.2
 point = 150
 
 # 計算
-beal_sum = beal_v * beal_c
+beer_sum = beer_v * beer_c
 otumami_sum = otumami_v * otumami_c
 yakitori_sum = yakitori_v * (1 - yakitori_rate) * yakitori_c
-payment = beal_sum + otumami_sum + yakitori_sum - point
+payment = beer_sum + otumami_sum + yakitori_sum - point
 
 # 結果を表示
 print("支払金額", payment, "円")
