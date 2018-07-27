2595a017fc222a62a097a92b650245bfabe69f51
n18010 <n18010@std.it-college.ac.jp>
2018-07-27 13:21:02 +0900

    7/26 課題Lv.1 新規作成

---
+++ src/basic-c3/task20180726_lv1.py
@@ -0,0 +1,50 @@
+# 自動双六プログラム ゴールちょうどじゃないバージョン
+
+
+def shake_dice():
+    """
+    1から6のサイコロの目をランダムに出す。
+
+    Parameters
+    ----------
+    なし
+
+    Returns
+    -------
+    dice : int
+        1から6のランダムな値
+    """
+    from random import randint
+    global dice
+    dice = randint(1, 6)
+    return dice
+
+
+def go_forward(n):
+    """
+    前に進んだ値の合計を出す。
+
+    Parameters
+    ----------
+    n : int
+        前に進む値
+
+    Returns
+    -------
+    なし
+    """
+    global cur_x
+    cur_x += n
+
+
+goal = 10  # 双六のゴール値
+
+cur_x = 0  # 前に進んだ値の合計のための変数
+
+while cur_x < goal:
+    input("サイコロを振ってください")
+    go_forward(shake_dice())
+    if cur_x < goal:
+        print("{0}が出ました。現在位置は{1}です。".format(dice, cur_x))
+
+print("{0}が出ました。おめでとうございます、ゴールしました！".format(dice))
