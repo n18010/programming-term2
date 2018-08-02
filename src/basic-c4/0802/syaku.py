b7379cd87c2585c7185db2c405470eb386efa036
n18010 <n18010@std.it-college.ac.jp>
2018-08-02 16:02:07 +0900

    8/2 授業

---
+++ src/basic-c4/0802/syaku.py
@@ -0,0 +1,8 @@
+# 尺からcmへの単位変換
+def syaku_to_cm(syaku):
+    return round(syaku * 30.303, 3)
+
+
+print(__name__)
+if __name__ == '__main__':
+    print(syaku_to_cm(30))
---
+++ src/basic-c4/0802/test-syaku2.py
@@ -0,0 +1,4 @@
+import mod.syaku
+
+print("10尺=", mod.syaku.syaku_to_cm(15), "cm")
+print("20尺=", mod.syaku.syaku_to_cm(30), "cm")
