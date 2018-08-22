ea91bd665fb9c5d87380fafe1223808a07094713
n18010 <n18010@std.it-college.ac.jp>
2018-08-22 17:02:12 +0900

    8/22 授業

---
+++ src/basic-c5/cgi-bin/show-params.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import cgi
+
+# ヘッダの表示
+print("Content-Type: text/html; charset=utf-8")
+print("")
+
+print("<pre>")
+# URLパラメータを取得する
+form = cgi.FieldStorage()
+
+# 特定のパラメータを取得して表示
+mode = form.getvalue("mode", default="")
+print("mode=", mode)
+
+# すべてのパラメータを取得して表示
+print("--- all params ---")
+for k in form.keys():
+    print(k, "=", form.getvalue(k))
+
+print("</pre>")
